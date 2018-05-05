221477771f63ff2f6773752d276e5c33bcecf1a2
Kristiyan <kristiyan.r.bogdanov@gmail.com>
2018-05-05 15:12:52 +0100

    some more editing

---
+++ src/theme/Globalstyle.js
@@ -0,0 +1,10 @@
+import { injectGlobal } from "styled-components";
+
+
+injectGlobal`
+     @import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+    body {
+        font-family: 'Roboto Mono', monospace;
+        font-size: 12px;
+    };
+`
