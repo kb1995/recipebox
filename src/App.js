aad0788ccf87cbc2cbfab3053fe498082fce0811
Kristiyan <kristiyan.r.bogdanov@gmail.com>
2018-05-05 16:05:45 +0100

    setting up parts of the UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
+import { Div } from './App.styled'
 
 class App extends Component {
+  state = {
+    title: "",
+    ingredients: "",
+    isclicked: false,
+  }
+
+  handleClick = () => {
+    this.setState({
+      isclicked: true,
+    })
+  }
+
+  handleTitle = (e) => {
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+  handleIngredients = (e) => {
+    this.setState({
+      ingredients: e.target.value
+    })
+  }
+
   render() {
+    if (this.state.isclicked) {
+      return (
+        <Div>
+          <button onClick={this.handleClick}>Add recipe</button>
+          <form>
+            <h3>Recipe Title:</h3>
+            <input onChange={this.handleTitle} type="text"></input>
+
+            <h3>Ingredients:</h3>
+            <input onChange={this.handleIngredients} type="text"></input>
+          </form>
+        </Div>
+      )
+    }
     return (
-      <div className="App">
-        Hello
-      </div>
+      <Div>
+        <button onClick={this.handleClick}>Add recipe</button>
+      </Div>
     );
   }
 }
